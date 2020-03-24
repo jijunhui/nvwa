3c0f0fe83401eaa186071deeab693f2c2cf414db
Jeffrey <jeffreyji@aliyun.com>
2020-03-24 22:29:03 +0800

    搭建项目架构 springboot + dubbo

---
+++ nvwa-services/nvwa-user-service/src/main/java/cn/home/jeffrey/user/UserApplication.java
@@ -0,0 +1,17 @@
+package cn.home.jeffrey.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jijunhui
+ * @version 1.0.0
+ * @date 2020/3/22 23:55
+ * @description 用户服务启动类
+ */
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
